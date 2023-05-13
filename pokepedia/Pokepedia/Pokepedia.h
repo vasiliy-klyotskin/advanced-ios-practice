56a952f804b3e9956c28a754ed1482a5cb4b905a
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-05-13 16:19:06 +0300

    xcode project init

---
+++ pokepedia/Pokepedia/Pokepedia.h
@@ -0,0 +1,18 @@
+//
+//  Pokepedia.h
+//  Pokepedia
+//
+//  Created by Василий Клецкин on 13.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pokepedia.
+FOUNDATION_EXPORT double PokepediaVersionNumber;
+
+//! Project version string for Pokepedia.
+FOUNDATION_EXPORT const unsigned char PokepediaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pokepedia/PublicHeader.h>
+
+
