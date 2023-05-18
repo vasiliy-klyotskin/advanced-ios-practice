d0fba64467e007c214644e34625a5f3ccf62ca05
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-05-18 10:17:41 -0700

    retrieves data test

--- pokepedia/PokepediaTests/Shared Cache/LocalLoaderTests.swift
+++ pokepedia/PokepediaTests/Shared Cache/LocalLoaderTests.swift
@@ -8,21 +8,43 @@
 import XCTest
 
 final class LocalLoader {
+    private let store: StoreMock
+    
     init(store: StoreMock) {
-        
+        self.store = store
+    }
+    
+    func load() {
+        store.retrieve()
     }
 }
 
 final class LocalLoaderTests: XCTestCase {
-    func test_init_hasNoSideEffects() throws {
+    func test_init_hasNoSideEffects() {
         let store = StoreMock()
         
         _ = LocalLoader(store: store)
         
         XCTAssertEqual(store.messages.count, 0)
     }
+    
+    func test_load_retrievesData() {
+        let store = StoreMock()
+        let sut = LocalLoader(store: store)
+        
+        sut.load()
+        
+        XCTAssertEqual(store.messages, [.retrieve])
+    }
 }
 
 final class StoreMock {
-    var messages: [Any] = []
+    enum Message {
+        case retrieve
+    }
+    var messages: [Message] = []
+    
+    func retrieve() {
+        messages.append(.retrieve)
+    }
 }
