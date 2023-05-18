3f4b94471da690bee85ca47c0ce9d56203e03550
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-05-18 11:31:27 -0700

    delivers error on expired cache test

--- pokepedia/PokepediaTests/Shared Cache/LocalLoaderTests.swift
+++ pokepedia/PokepediaTests/Shared Cache/LocalLoaderTests.swift
@@ -7,15 +7,32 @@
 
 import XCTest
 
+typealias TimestampValidation = (_ timestamp: Date, _ against: Date) -> Bool
+
+struct StoreRetrieval {
+    let timestamp: Date
+}
+
 final class LocalLoader {
     private let store: StoreMock
+    private let validation: TimestampValidation
+    private let current: () -> Date
     
-    init(store: StoreMock) {
+    init(
+        store: StoreMock,
+        validation: @escaping TimestampValidation,
+        current: @escaping () -> Date
+    ) {
         self.store = store
+        self.validation = validation
+        self.current = current
     }
     
     func load() throws {
-        try store.retrieve()
+        let result = try store.retrieve()
+        if !validation(result.timestamp, current()) {
+            throw NSError()
+        }
     }
 }
 
@@ -26,17 +43,16 @@ final class LocalLoaderTests: XCTestCase {
         XCTAssertEqual(store.messages.count, 0)
     }
     
-    func test_load_retrievesData() throws {
+    func test_load_retrievesData() {
         let (sut, store) = makeSut()
         
-        try sut.load()
+        try? sut.load()
         
         XCTAssertEqual(store.messages, [.retrieve])
     }
     
-    func test_load_deliversErrorOnRetrievalError() throws {
+    func test_load_deliversErrorOnRetrievalError() {
         let (sut, store) = makeSut()
-        
         store.stubRetrieve(result: .failure(anyNSError()))
         
         XCTAssertThrowsError(
@@ -44,30 +60,96 @@ final class LocalLoaderTests: XCTestCase {
         )
     }
     
+    func test_load_deliversErrorOnExpiredCache() {
+        let current = Date()
+        let timestamp = anyPreviousDate()
+        let expiredValidation = validation(
+            expired: true,
+            current: current,
+            timestamp: timestamp
+        )
+        let (sut, store) = makeSut(current: current, validate: expiredValidation)
+        
+        store.stubRetrieve(result: .success(.init(timestamp: timestamp)))
+        
+        XCTAssertThrowsError(
+            try sut.load()
+        )
+        
+    }
+    
     // MARK: - Helpers
     
-    private func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (LocalLoader, StoreMock) {
+    private func makeSut(
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        current: Date = .init(),
+        validate: @escaping TimestampValidation = { _, _ in false }
+    ) -> (LocalLoader, StoreMock) {
         let store = StoreMock()
-        let sut = LocalLoader(store: store)
+        let sut = LocalLoader(
+            store: store,
+            validation: validate,
+            current: { current }
+        )
         trackForMemoryLeaks(store)
         trackForMemoryLeaks(sut)
         return (sut, store)
     }
+    
+    private func validation(
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        expired: Bool,
+        current: Date,
+        timestamp: Date
+    ) -> TimestampValidation {
+        { actualTimestamp, actualCurrent in
+            XCTAssertEqual(
+                actualTimestamp,
+                timestamp,
+                "Validation should be called with right timestamp",
+                file: file,
+                line: line
+            )
+            XCTAssertEqual(
+                actualCurrent,
+                current,
+                "Validation should be called with right current date",
+                file: file,
+                line: line
+            )
+            return !expired
+        }
+    }
+    
+    private func anyPreviousDate() -> Date {
+        adding(days: -1, to: Date())
+    }
+    
+    private func adding(days: Int, to date: Date) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: date)!
+    }
 }
 
 final class StoreMock {
+    struct Unexpected: Error {}
     enum Message {
         case retrieve
     }
-    var messages: [Message] = []
-    var retrieveStub: Result<Any, Error>?
     
-    func retrieve() throws {
+    var messages: [Message] = []
+    var retrieveStub: Result<StoreRetrieval, Error>?
+    
+    func retrieve() throws -> StoreRetrieval {
         messages.append(.retrieve)
-        _ = try retrieveStub?.get()
+        if let stub = retrieveStub {
+            return try stub.get()
+        }
+        throw Unexpected()
     }
     
-    func stubRetrieve(result: Result<Any, Error>) {
+    func stubRetrieve(result: Result<StoreRetrieval, Error>) {
         retrieveStub = result
     }
 }
