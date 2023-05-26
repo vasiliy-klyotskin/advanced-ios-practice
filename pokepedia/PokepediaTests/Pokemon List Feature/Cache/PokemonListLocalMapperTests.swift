08d576b17257c54273fad5aba9f5fe0ad62ab275
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-05-26 09:54:50 -0700

    test file for local mapper

---
+++ pokepedia/PokepediaTests/Pokemon List Feature/Cache/PokemonListLocalMapperTests.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonListLocalMapperTests.swift
+//  PokepediaTests
+//
+//  Created by Василий Клецкин on 5/26/23.
+//
+
+import XCTest
+
+final class PokemonListLocalMapperTests: XCTestCase {
+    func testExample() throws {
+
+    }
+}
