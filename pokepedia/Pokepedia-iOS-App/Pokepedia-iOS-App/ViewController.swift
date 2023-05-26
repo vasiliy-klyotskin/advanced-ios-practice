61ef341d01b14c10b1a60a4894c238ef688f6b82
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-05-26 10:55:42 -0700

    add workspace and create project for ios composition root

---
+++ pokepedia/Pokepedia-iOS-App/Pokepedia-iOS-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokepedia-iOS-App
+//
+//  Created by Василий Клецкин on 5/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
