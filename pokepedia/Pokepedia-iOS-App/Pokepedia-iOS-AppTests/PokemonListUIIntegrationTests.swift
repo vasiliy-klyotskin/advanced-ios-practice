5c332e498e9dd018525c075228db218223c59c74
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-05-28 01:14:12 -0700

    add test target to main project for integration tests

---
+++ pokepedia/Pokepedia-iOS-App/Pokepedia-iOS-AppTests/PokemonListUIIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonListUIIntegrationTests.swift
+//  Pokepedia-iOS-AppTests
+//
+//  Created by Василий Клецкин on 5/28/23.
+//
+
+import XCTest
+
+final class PokemonListUIIntegrationTests: XCTestCase {
+    func testExample() throws {
+        
+    }
+}
