95619249f135cbf59be3dd0850a5cb81498f0524
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-07-28 15:44:13 +0400

    pokemon list presenter map test

--- pokepedia/PokepediaTests/Pokemon List Feature/Presentation/PokemonListPresenterTests.swift
+++ pokepedia/PokepediaTests/Pokemon List Feature/Presentation/PokemonListPresenterTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-import Pokepedia
+@testable import Pokepedia
 
 final class PokemonListPresenterTests: XCTestCase {
     func test_title_isLocalized() {
@@ -19,5 +19,44 @@ final class PokemonListPresenterTests: XCTestCase {
         let actual = PokemonListPresenter.title
         
         XCTAssertEqual(actual, expected)
+    }
+    
+    func test_map_deliversViewModel() {
+        let (viewModel, model) = makeItem()
+        
+        let result = PokemonListPresenter.map(item: model, colorMapping: Color.init)
+        
+        XCTAssertEqual(result, viewModel)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeItem() -> (ListPokemonItemViewModel<Color>, PokemonListItem) {
+        let viewModel = ListPokemonItemViewModel<Color>(
+            name: "some name",
+            id: "some id",
+            physicalType: "some physical type",
+            specialType: "some special type",
+            physicalTypeColor: Color(value: "some physical color"),
+            specialTypeColor: Color(value: "some special color")
+        )
+        let model = PokemonListItem(
+            id: "some id",
+            name: "some name",
+            imageUrl: anyURL(),
+            physicalType: .init(
+                color: "some physical color",
+                name: "some physical type"
+            ),
+            specialType: .init(
+                color: "some special color",
+                name: "some special type")
+        )
+        
+        return (viewModel, model)
+    }
+    
+    private struct Color: Hashable {
+        let value: String
     }
 }
