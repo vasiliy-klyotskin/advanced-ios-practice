e1a21ccbb3bf0580fd219ba510adb9bc68e3e394
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-07-31 20:00:16 +0400

    load requests cache retrieval test

--- pokepedia/PokepediaTests/Pokemon List Feature/Cache/LoadPokemonListFromCacheUseCaseTests.swift
+++ pokepedia/PokepediaTests/Pokemon List Feature/Cache/LoadPokemonListFromCacheUseCaseTests.swift
@@ -7,8 +7,20 @@
 
 import XCTest
 
+protocol LocalPokemonListStore {
+    func retrieve()
+}
+
 final class LocalPokemonListLoader {
+    private let store: LocalPokemonListStore
     
+    init(store: LocalPokemonListStore) {
+        self.store = store
+    }
+    
+    func load() {
+        store.retrieve()
+    }
 }
 
 final class LoadPokemonListFromCacheUseCaseTests: XCTestCase {
@@ -18,17 +30,33 @@ final class LoadPokemonListFromCacheUseCaseTests: XCTestCase {
         XCTAssertTrue(store.receivedMessages.isEmpty)
     }
     
+    func test_load_requestsCacheRetrieval() {
+        let (sut, store) = makeSut()
+        
+        sut.load()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieval])
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (LocalPokemonListLoader, PokemonListStoreMock) {
         let mock = PokemonListStoreMock()
-        let sut = LocalPokemonListLoader()
+        let sut = LocalPokemonListLoader(store: mock)
         trackForMemoryLeaks(mock, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, mock)
     }
 }
 
-final class PokemonListStoreMock {
-    var receivedMessages: [Any] = []
+final class PokemonListStoreMock: LocalPokemonListStore {
+    enum Message {
+        case retrieval
+    }
+    
+    var receivedMessages: [Message] = []
+    
+    func retrieve() {
+        receivedMessages.append(.retrieval)
+    }
 }
