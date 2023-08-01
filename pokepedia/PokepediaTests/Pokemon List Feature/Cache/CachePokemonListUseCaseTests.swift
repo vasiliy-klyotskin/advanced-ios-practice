cd4b44a6cf3b7e071641dfd4eebfca259855af38
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-08-01 18:32:00 +0400

    cache pokemon list use case tests: init does not message store upon creation test

---
+++ pokepedia/PokepediaTests/Pokemon List Feature/Cache/CachePokemonListUseCaseTests.swift
@@ -0,0 +1,31 @@
+//
+//  CachePokemonListUseCaseTests.swift
+//  PokepediaTests
+//
+//  Created by Василий Клецкин on 8/1/23.
+//
+
+import XCTest
+import Pokepedia
+
+final class CachePokemonListUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSut()
+        
+        XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSut(
+        currentDate: () -> Date = Date.init,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (LocalPokemonListLoader, PokemonListStoreMock) {
+        let store = PokemonListStoreMock()
+        let sut = LocalPokemonListLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
