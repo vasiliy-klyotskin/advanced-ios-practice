48abc2bf88b5168bc8ee26e2a8ea1a743fdd6826
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-08-02 22:19:00 +0400

    retrieve has no side effects on empty cache

--- pokepedia/PokepediaTests/Pokemon List Feature/Cache Infra/CoreDataLocalPokemonListStoreTests.swift
+++ pokepedia/PokepediaTests/Pokemon List Feature/Cache Infra/CoreDataLocalPokemonListStoreTests.swift
@@ -24,20 +24,41 @@ final class CoreDataLocalPokemonListStore: LocalPokemonListStore {
 
 final class CoreDataPokemonListLocalStoreTests: XCTestCase {
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = makeSUT()
+        let sut = makeSut()
         
         expect(sut, toRetrieve: .success(nil))
     }
     
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSut()
+        
+        expect(sut, toRetrieveTwice: .success(nil))
+    }
+    
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalPokemonListStore {
+    private func makeSut(file: StaticString = #filePath, line: UInt = #line) -> LocalPokemonListStore {
         let sut = CoreDataLocalPokemonListStore()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
     
-    private func expect(_ sut: LocalPokemonListStore, toRetrieve expectedResult: Result<CachedPokemonList?, Error>, file: StaticString = #filePath, line: UInt = #line) {
+    private func expect(
+        _ sut: LocalPokemonListStore,
+        toRetrieveTwice expectedResult: Result<CachedPokemonList?, Error>,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+    }
+    
+    private func expect(
+        _ sut: LocalPokemonListStore,
+        toRetrieve expectedResult: Result<CachedPokemonList?, Error>,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
         let retrievedResult = Result { try sut.retrieve() }
         switch (expectedResult, retrievedResult) {
         case (.success(.none), .success(.none)),
