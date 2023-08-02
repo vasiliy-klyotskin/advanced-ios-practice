7e6df574df9e26fc7d734239ac4431fbdfdb75fc
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-08-02 22:16:41 +0400

    core data local pokemon list store: retrieve delivers empty on empty cache

---
+++ pokepedia/PokepediaTests/Pokemon List Feature/Cache Infra/CoreDataLocalPokemonListStoreTests.swift
@@ -0,0 +1,54 @@
+//
+//  CoreDataLocalPokemonListStoreTests.swift
+//  PokepediaTests
+//
+//  Created by Василий Клецкин on 8/2/23.
+//
+
+import XCTest
+import Pokepedia
+
+final class CoreDataLocalPokemonListStore: LocalPokemonListStore {
+    func retrieve() throws -> Pokepedia.CachedPokemonList? {
+        nil
+    }
+    
+    func delete() throws {
+        
+    }
+    
+    func insert(local: Pokepedia.LocalPokemonList, timestamp: Date) throws {
+        
+    }
+}
+
+final class CoreDataPokemonListLocalStoreTests: XCTestCase {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+        
+        expect(sut, toRetrieve: .success(nil))
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalPokemonListStore {
+        let sut = CoreDataLocalPokemonListStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func expect(_ sut: LocalPokemonListStore, toRetrieve expectedResult: Result<CachedPokemonList?, Error>, file: StaticString = #filePath, line: UInt = #line) {
+        let retrievedResult = Result { try sut.retrieve() }
+        switch (expectedResult, retrievedResult) {
+        case (.success(.none), .success(.none)),
+             (.failure, .failure):
+            break
+            
+        case let (.success(.some(expected)), .success(.some(retrieved))):
+            XCTAssertEqual(retrieved.local, expected.local, file: file, line: line)
+            XCTAssertEqual(retrieved.timestamp, expected.timestamp, file: file, line: line)
+        default:
+            XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
+        }
+    }
+}
