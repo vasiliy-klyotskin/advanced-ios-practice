347e2d0e309a8b2987b526b2e14f1d75e8ca9d42
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-08-03 21:23:54 +0400

    load requests stored data for url test

--- pokepedia/PokepediaTests/Pokemon List Feature/Cache/LoadPokemonListImageFromCacheUseCaseTests.swift
+++ pokepedia/PokepediaTests/Pokemon List Feature/Cache/LoadPokemonListImageFromCacheUseCaseTests.swift
@@ -9,7 +9,15 @@ import XCTest
 import Pokepedia
 
 final class PokemonListimageLoader {
+    private let store: PokemonListImageStoreSpy
+
+    init(store: PokemonListImageStoreSpy) {
+        self.store = store
+    }
     
+    func loadImageData(from url: URL) throws -> Any? {
+        store.retrieveImage(for: url)
+    }
 }
 
 final class LoadPokemonListImageFromCacheUseCaseTests: XCTestCase {
@@ -18,11 +26,21 @@ final class LoadPokemonListImageFromCacheUseCaseTests: XCTestCase {
         
         XCTAssertTrue(store.receivedMessages.isEmpty)
     }
+    
+    func test_load_requestsStoredDataForURL() {
+        let (sut, store) = makeSut()
+        let url = anyURL()
+        
+        _ = try? sut.loadImageData(from: url)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (PokemonListimageLoader, PokemonListImageStoreSpy) {
         let store = PokemonListImageStoreSpy()
-        let sut = PokemonListimageLoader()
+        let sut = PokemonListimageLoader(store: store)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
@@ -30,5 +48,14 @@ final class LoadPokemonListImageFromCacheUseCaseTests: XCTestCase {
 }
 
 final class PokemonListImageStoreSpy {
-    var receivedMessages: [Any] = []
+    enum Message: Equatable {
+        case retrieve(dataFor: URL)
+    }
+    
+    var receivedMessages: [Message] = []
+    
+    func retrieveImage(for url: URL) -> Data? {
+        receivedMessages.append(.retrieve(dataFor: url))
+        return nil
+    }
 }
