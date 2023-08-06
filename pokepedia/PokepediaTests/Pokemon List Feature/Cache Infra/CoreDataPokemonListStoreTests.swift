59572c9d7eeecc412777c73d298e704b6d43d548
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-08-06 09:42:04 +0400

    remove redundant helpers

--- pokepedia/PokepediaTests/Pokemon List Feature/Cache Infra/CoreDataPokemonListStoreTests.swift
+++ pokepedia/PokepediaTests/Pokemon List Feature/Cache Infra/CoreDataPokemonListStoreTests.swift
@@ -137,34 +137,4 @@ final class CoreDataPokemonListLocalStoreTests: XCTestCase, FailbalePokemonListS
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
-    
-    private func expect(
-        _ sut: PokemonListStore,
-        toRetrieveTwice expectedResult: Result<CachedPokemonList?, Error>,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        expect(sut, toRetrieve: expectedResult, file: file, line: line)
-        expect(sut, toRetrieve: expectedResult, file: file, line: line)
-    }
-    
-    private func expect(
-        _ sut: PokemonListStore,
-        toRetrieve expectedResult: Result<CachedPokemonList?, Error>,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        let retrievedResult = Result { try sut.retrieve() }
-        switch (expectedResult, retrievedResult) {
-        case (.success(.none), .success(.none)),
-             (.failure, .failure):
-            break
-            
-        case let (.success(.some(expected)), .success(.some(retrieved))):
-            XCTAssertEqual(retrieved.local, expected.local, file: file, line: line)
-            XCTAssertEqual(retrieved.timestamp, expected.timestamp, file: file, line: line)
-        default:
-            XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
-        }
-    }
 }
