59ae195924f1b4b31fde864d37574485be6fa960
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-08-15 09:51:53 +0400

    make creates url request after specific item

--- pokepedia/PokepediaTests/Pokemon List Feature/API/PokemonListEndpointTest.swift
+++ pokepedia/PokepediaTests/Pokemon List Feature/API/PokemonListEndpointTest.swift
@@ -9,18 +9,21 @@ import XCTest
 import Pokepedia
 
 enum PokemonListEndpoint {
-    case get
+    case get(after: PokemonListItem? = nil)
     
     func make(with baseUrl: URL) -> URLRequest {
-        var components = URLComponents()
-        components.scheme = baseUrl.scheme
-        components.host = baseUrl.host
-        components.path = baseUrl.path + "/list"
-        components.queryItems = [
-            .init(name: "after_id", value: "0"),
-            .init(name: "limit", value: "20")
-        ]
-        return URLRequest(url: components.url!)
+        switch self {
+        case .get(let item):
+            var components = URLComponents()
+            components.scheme = baseUrl.scheme
+            components.host = baseUrl.host
+            components.path = baseUrl.path + "/list"
+            components.queryItems = [
+                .init(name: "after_id", value: "\(item?.id ?? 0)"),
+                .init(name: "limit", value: "20")
+            ]
+            return URLRequest(url: components.url!)
+        }
     }
 }
 
@@ -28,12 +31,31 @@ final class PokemonListEndpointTest: XCTestCase {
     func test_make_createsUrlRequestForAllList() {
         let baseURL = URL(string: "http://base-url.com")!
         
-        let received = PokemonListEndpoint.get.make(with: baseURL)
+        let received = PokemonListEndpoint.get().make(with: baseURL)
         
         XCTAssertEqual(received.url?.scheme, "http", "scheme")
         XCTAssertEqual(received.url?.host, "base-url.com", "host")
         XCTAssertEqual(received.url?.path, "/list", "path")
         XCTAssertEqual(received.url?.query, "after_id=0&limit=20", "query")
         XCTAssertEqual(received.httpMethod, "GET")
+    }
+    
+    func test_make_createsUrlRequestAfterSpecificItem() {
+        let item = anyItem(withId: 17)
+        let baseURL = URL(string: "http://base-url.com")!
+        
+        let received = PokemonListEndpoint.get(after: item).make(with: baseURL)
+        
+        XCTAssertEqual(received.url?.scheme, "http", "scheme")
+        XCTAssertEqual(received.url?.host, "base-url.com", "host")
+        XCTAssertEqual(received.url?.path, "/list", "path")
+        XCTAssertEqual(received.url?.query, "after_id=17&limit=20", "query")
+        XCTAssertEqual(received.httpMethod, "GET")
+    }
+    
+    // MARK: - Helpers
+    
+    private func anyItem(withId id: Int) -> PokemonListItem {
+        PokemonListItem(id: id, name: "any name", imageUrl: anyURL(), physicalType: .init(color: "any color", name: "any name"), specialType: nil)
     }
 }
