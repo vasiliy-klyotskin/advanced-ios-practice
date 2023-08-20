9d3927358297b407d0266417f5bf33ba84ab54b0
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-08-20 10:45:33 +0400

    make creates url request test

---
+++ pokepedia/Pokepedia/Pokemon Detail Feature/API/DetailPokemonEndpoint.swift
@@ -0,0 +1,29 @@
+//
+//  DetailPokemonEndpoint.swift
+//  Pokepedia
+//
+//  Created by Василий Клецкин on 8/20/23.
+//
+
+import Foundation
+
+public enum DetailPokemonEndpoint {
+    public typealias ID = Int
+    
+    case get(ID)
+    
+    public func make(with baseUrl: URL) -> URLRequest {
+        switch self {
+        case .get(let id):
+            var components = URLComponents()
+            components.port = baseUrl.port
+            components.scheme = baseUrl.scheme
+            components.host = baseUrl.host
+            components.path = baseUrl.path + "/detail"
+            components.queryItems = [
+                .init(name: "id", value: "\(id)"),
+            ]
+            return URLRequest(url: components.url!)
+        }
+    }
+}
---
+++ pokepedia/PokepediaTests/Pokemon Detail Feature/API/DetailPokemonEndpointTests.swift
@@ -0,0 +1,28 @@
+//
+//  DetailPokemonEndpointTests.swift
+//  PokepediaTests
+//
+//  Created by Василий Клецкин on 8/20/23.
+//
+
+import XCTest
+import Pokepedia
+
+final class DetailPokemonEndpointTests: XCTestCase {
+    func test_make_createsUrlRequest() {
+        let baseURL = URL(string: "http://base-url.com:8080")!
+        let ids = [0, 1, 2]
+        
+        ids.forEach { id in
+            let received = DetailPokemonEndpoint.get(id).make(with: baseURL)
+            
+            XCTAssertEqual(received.url?.port, baseURL.port)
+            XCTAssertEqual(received.url?.scheme, "http", "scheme for \(id)")
+            XCTAssertEqual(received.url?.host, "base-url.com", "host for \(id)")
+            XCTAssertEqual(received.url?.path, "/detail", "path for \(id)")
+            XCTAssertEqual(received.url?.port, 8080, "port for \(id)")
+            XCTAssertEqual(received.url?.query, "id=\(id)", "query for \(id)")
+            XCTAssertEqual(received.httpMethod, "GET", "method for \(id)")
+        }
+    }
+}
