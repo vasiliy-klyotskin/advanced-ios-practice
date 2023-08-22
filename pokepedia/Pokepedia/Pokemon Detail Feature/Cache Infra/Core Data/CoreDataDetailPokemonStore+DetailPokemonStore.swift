1efa2a170f0ba554ec845eaac1d95075c2eea7ae
Vasiliy Klyotskin <vasiliy@klyotskin.ru>
2023-08-22 19:57:17 +0400

    extract shared helpers and small refactoring

---
+++ pokepedia/Pokepedia/Pokemon Detail Feature/Cache Infra/Core Data/CoreDataDetailPokemonStore+DetailPokemonStore.swift
@@ -0,0 +1,44 @@
+//
+//  CoreDataDetailPokemonStore+DetailPokemonStore.swift
+//  Pokepedia
+//
+//  Created by Василий Клецкин on 8/22/23.
+//
+
+import Foundation
+
+extension CoreDataDetailPokemonStore: DetailPokemonStore {
+    public func retrieveForValidation() throws -> [DetailPokemonValidationRetrieval] {
+        try performSync { context in
+            try ManagedDetailPokemonCache.retrievals(in: context)
+        }
+    }
+    
+    public func deleteAll() {
+        try? performSync { context in
+            ManagedDetailPokemonCache.deleteCache(in: context)
+        }
+    }
+    
+    public func retrieve(for id: Int) -> DetailPokemonCache? {
+        try? performSync { context in
+            let managedPokemon = ManagedDetailPokemon.first(with: id, in: context)
+            return managedPokemon.map { .init(timestamp: $0.timestamp, local: $0.local) }
+        }
+    }
+    
+    public func delete(for id: Int) {
+        try? performSync { context in
+            ManagedDetailPokemon.delete(for: id, in: context)
+        }
+    }
+    
+    public func insert(_ cache: DetailPokemonCache, for id: Int) {
+        try? performSync { context in
+            let container = ManagedDetailPokemonCache.instance(in: context)
+            let managedPokemon = cache.managedPokemon(id: id, context: context)
+            managedPokemon.cache = container
+            try? context.save()
+        }
+    }
+}
--- pokepedia/Pokepedia/Pokemon Detail Feature/Cache Infra/Core Data/CoreDataDetailPokemonStore.swift
+++ pokepedia/Pokepedia/Pokemon Detail Feature/Cache Infra/Core Data/CoreDataDetailPokemonStore.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreData
 
-public final class CoreDataDetailPokemonStore: DetailPokemonStore {
+public final class CoreDataDetailPokemonStore {
     private static let modelName = "DetailPokemonStore"
     private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataPokemonListStore.self))
 
@@ -23,47 +23,12 @@ public final class CoreDataDetailPokemonStore: DetailPokemonStore {
         guard let model = CoreDataDetailPokemonStore.model else {
             throw ModelNotFound(modelName: CoreDataDetailPokemonStore.modelName)
         }
-
         container = try NSPersistentContainer.load(
             name: CoreDataDetailPokemonStore.modelName,
             model: model,
             url: storeUrl
         )
         context = container.newBackgroundContext()
-    }
-    
-    public func retrieveForValidation() throws -> [DetailPokemonValidationRetrieval] {
-        try performSync { context in
-            try ManagedDetailPokemonCache.retrievals(in: context)
-        }
-    }
-    
-    public func deleteAll() {
-        try? performSync { context in
-            ManagedDetailPokemonCache.deleteCache(in: context)
-        }
-    }
-    
-    public func retrieve(for id: Int) -> DetailPokemonCache? {
-        try? performSync { context in
-            let managedPokemon = ManagedDetailPokemon.first(with: id, in: context)
-            return managedPokemon.map { .init(timestamp: $0.timestamp, local: $0.local) }
-        }
-    }
-    
-    public func delete(for id: Int) {
-        try? performSync { context in
-            ManagedDetailPokemon.delete(for: id, in: context)
-        }
-    }
-    
-    public func insert(_ cache: DetailPokemonCache, for id: Int) {
-        try? performSync { context in
-            let container = ManagedDetailPokemonCache.instance(in: context)
-            let managedPokemon = cache.managedPokemon(id: id, context: context)
-            managedPokemon.cache = container
-            try? context.save()
-        }
     }
     
     func performSync<R>(_ action: (NSManagedObjectContext) throws -> R) throws -> R {
